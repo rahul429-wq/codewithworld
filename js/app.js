af436a7b208df81921453255e42dde46a0d96b08
rahulprajapati1665@gmail.com <rahulprajapati1665@gmail.com>
2021-07-04 15:02:36 +0530

    initial

---
+++ js/app.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+  var a = new Date();
+  var days = new Array(7);
+  days[0] = "Sunday";
+  days[1] = "Monday";
+  days[2] = "Tuesday";
+  days[3] = "Wednesday";
+  days[4] = "Thursday";
+  days[5] = "Friday";
+  days[6] = "Saturday";
+  var r = days[a.getDay()];
+  document.getElementById("dayname").innerHTML = r;
+};
+// !menu
+
+let ham = document.getElementById("hamburger");
+let sidebar = document.querySelector(".sidebar");
+
+ham.addEventListener("click", () => {
+  sidebar.classList.toggle("toggle");
+});
+
+// !scrolltop
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
